48804fd290d0be566e1f379706678ed6d4dad950
dagasatvik10 <dagasatvik10@gmail.com>
2019-09-21 17:19:35 +0530

    Added syn command for cli

--- actions.js
+++ actions.js
@@ -1,5 +1,6 @@
 const ora = require('ora');
 const logger = require('./logger');
+const { parseRelatedWordsResponse } = require('./helpers');
 const { getDefinitions, getExamples, getRandomWord, getRelatedWords } = require('./services');
 
 exports.defAction = async word => {
@@ -10,6 +11,25 @@ exports.defAction = async word => {
     result.forEach(defs => {
       logger.success(defs.text);
     });
+  } catch (err) {
+    spinner.stop();
+    logger.error(err);
+  }
+};
+
+exports.synAction = async word => {
+  const spinner = ora('Loading Synonyms').start();
+  try {
+    const result = await getRelatedWords(word);
+    spinner.stop();
+    const parsedResult = parseRelatedWordsResponse(result);
+    if (parsedResult && parsedResult.synonym && parsedResult.synonym.length) {
+      parsedResult.synonym.forEach(s => {
+        logger.success(s);
+      });
+    } else {
+      logger.error(`No synonyms for ${word}`);
+    }
   } catch (err) {
     spinner.stop();
     logger.error(err);
--- index.js
+++ index.js
@@ -3,13 +3,20 @@
 // load environment variables
 require('dotenv').config();
 const com = require('commander');
-const { defAction } = require('./actions');
+const { defAction, synAction } = require('./actions');
 
 com
   .version('1.0.0')
   .command('def <word>')
   .action(async word => {
     defAction(word);
+  });
+
+com
+  .version('1.0.0')
+  .command('syn <word>')
+  .action(async word => {
+    synAction(word);
   });
 
 com.parse(process.argv);
