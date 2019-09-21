d80896e95e64ccf0238f071390e3f93d5824158d
dagasatvik10 <dagasatvik10@gmail.com>
2019-09-21 18:14:54 +0530

    Refactor code
    - added a common display function to handle errors
    - refactored actions to use diplay function

--- actions.js
+++ actions.js
@@ -1,59 +1,18 @@
-const logger = require('./logger');
-const { parseRelatedWordsResponse } = require('./helpers');
-const { getDefinitions, getExamples, getRandomWord, getRelatedWords } = require('./services');
+const { displayData, displayDef, displaySyn, displayAnt, displayEx } = require('./helpers');
+const { getRandomWord } = require('./services');
 
 exports.defAction = async word => {
-  try {
-    const result = await getDefinitions(word);
-    result.forEach(defs => {
-      logger.success(defs.text);
-    });
-  } catch (err) {
-    logger.error(err);
-  }
+  displayData(displayDef, word);
 };
 
 exports.synAction = async word => {
-  try {
-    const result = await getRelatedWords(word);
-    const parsedResult = parseRelatedWordsResponse(result);
-    if (parsedResult && parsedResult.synonym && parsedResult.synonym.length) {
-      parsedResult.synonym.forEach(s => {
-        logger.success(s);
-      });
-    } else {
-      logger.error(`No synonyms for ${word}`);
-    }
-  } catch (err) {
-    logger.error(err);
-  }
+  displayData(displaySyn, word);
 };
 
 exports.antAction = async word => {
-  try {
-    const result = await getRelatedWords(word);
-    const parsedResult = parseRelatedWordsResponse(result);
-    if (parsedResult && parsedResult.antonym && parsedResult.antonym.length) {
-      parsedResult.antonym.forEach(s => {
-        logger.success(s);
-      });
-    } else {
-      logger.error(`No synonyms for ${word}`);
-    }
-  } catch (err) {
-    logger.error(err);
-  }
+  displayData(displayAnt, word);
 };
 
 exports.exAction = async word => {
-  try {
-    const result = await getExamples(word);
-    if (result && result.examples) {
-      result.examples.forEach(e => {
-        logger.success(e.text);
-      });
-    }
-  } catch (err) {
-    logger.error(err);
-  }
+  displayData(displayEx, word);
 };
--- helpers.js
+++ helpers.js
@@ -1,4 +1,7 @@
-exports.parseRelatedWordsResponse = data => {
+const logger = require('./logger');
+const { getDefinitions, getExamples, getRelatedWords } = require('./services');
+
+const parseRelatedWordsResponse = data => {
   if (data && data.length) {
     const result = data.reduce((acc, v) => {
       acc[v.relationshipType] = v.words;
@@ -7,4 +10,62 @@ exports.parseRelatedWordsResponse = data => {
     return result;
   }
   return {};
+};
+
+exports.displayData = async (displayFn, params) => {
+  try {
+    if (params) {
+      await displayFn(params);
+    } else {
+      displayFn();
+    }
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+exports.displayDef = async word => {
+  const result = await getDefinitions(word);
+  if (result && result.length) {
+    result.forEach(defs => {
+      logger.success(defs.text);
+    });
+  } else {
+    throw new Error(`No definitions for ${word}`);
+  }
+};
+
+exports.displaySyn = async word => {
+  const result = await getRelatedWords(word);
+  const parsedResult = parseRelatedWordsResponse(result);
+  if (parsedResult && parsedResult.synonym && parsedResult.synonym.length) {
+    parsedResult.synonym.forEach(s => {
+      logger.success(s);
+    });
+  } else {
+    throw new Error(`No synonyms for ${word}`);
+  }
+};
+
+exports.displayAnt = async word => {
+  const result = await getRelatedWords(word);
+  const parsedResult = parseRelatedWordsResponse(result);
+  if (parsedResult && parsedResult.antonym && parsedResult.antonym.length) {
+    parsedResult.antonym.forEach(s => {
+      logger.success(s);
+    });
+  } else {
+    throw new Error(`No antonyms for ${word}`);
+  }
+};
+
+exports.displayEx = async word => {
+  const result = await getExamples(word);
+  if (result && result.examples) {
+    result.examples.forEach(e => {
+      logger.success(e.text);
+    });
+  } else {
+    throw new Error(`No examples for ${word}`);
+  }
 };
