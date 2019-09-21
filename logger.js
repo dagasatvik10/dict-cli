8d069db43cc426efd69955440aba5056987269c0
dagasatvik10 <dagasatvik10@gmail.com>
2019-09-21 13:23:23 +0530

    Added success, warn and error logger

---
+++ logger.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+
+const log = console.log;
+
+exports.success = msg => {
+  log(chalk.green(msg));
+};
+
+exports.error = msg => {
+  log(chalk.red(msg));
+};
+
+exports.warn = msg => {
+  log(chalk.yellow(msg));
+};
