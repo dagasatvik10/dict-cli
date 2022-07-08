50b564e9afc0a60deea76e41bb4013e7fa628a88
Satvik Daga <dagsatvik10@gmail.com>
2022-07-08 17:22:38 +0530

    refactor: Update loggers
    - add a util to remove html tags from message

--- logger.js
+++ logger.js
@@ -1,19 +1,42 @@
 const chalk = require('chalk');
+const { removeHtmlTags } = require('./utils');
 
-const log = console.log;
+/**
+ *
+ * @param {string} text
+ */
+const log = (text) => {
+  console.log(removeHtmlTags(text));
+};
 
+/**
+ * print success message(green color)
+ * @param {string} msg
+ */
 exports.success = (msg) => {
   log(chalk.green(msg));
 };
 
+/**
+ * print error message(red color)
+ * @param {string} msg
+ */
 exports.error = (msg) => {
   log(chalk.red(msg));
 };
 
+/**
+ * print warning message(yellow color)
+ * @param {string} msg
+ */
 exports.warn = (msg) => {
   log(chalk.yellow(msg));
 };
 
+/**
+ * print info message(default color)
+ * @param {string} msg
+ */
 exports.info = (msg) => {
   log(msg);
 };
